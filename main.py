9eb506ca5c69c02597091710410af265becb5451
Nikita Sukhonosov <sukhonosov.nikita@gmail.com>
2022-02-07 18:19:17 +0200

    First Word

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-def nearest_value(values: set, one: int) -> int:
-    # your code here
-    # result = 0
-    # min_diff = max(values) - one
-    # for i in values:
-    #     if len(values) == 1:
-    #         result = min(i, one)
-    #         break
-    #     if abs(one - i) <= min_diff:
-    #         min_diff = abs(one - i)
-    #         result = i
-    abs_diff_func = lambda list_value: abs(list_value - one)
-    closest_value = min(values, key=abs_diff_func)
-    return closest_value
+import re
+#
+#
+# def first_word(text: str) -> str:
+#     """
+#     returns the first word in a given text.
+#     """
+#     # your code here
+#     text_without_dots = re.sub('[$\w\s]', '', text)
+#     return text_without_dots.split()[0]
+#
+#
+# if __name__ == "__main__":
+#     print("Example:")
+#     print(first_word("Hello world"))
+#     print(first_word("don't touch it"))
+#
+#     # # These "asserts" are used for self-checking and not for an auto-testing
+#     # assert first_word("Hello world") == "Hello"
+#     # assert first_word(" a word ") == "a"
+#     # assert first_word("don't touch it") == "don't"
+#     # assert first_word("greetings, friends") == "greetings"
+#     # assert first_word("... and so on ...") == "and"
+#     # assert first_word("hi") == "hi"
+#     # assert first_word("Hello.World") == "Hello"
+#     # print("Coding complete? Click 'Check' to earn cool rewards!")
 
-
-if __name__ == "__main__":
-    print("Example:")
-    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
-    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
-    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
-    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
-    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
-    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
-    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
-    assert nearest_value({5}, 5) == 5
-    assert nearest_value({5}, 7) == 5
-    assert nearest_value({0, -2}, -1) == -2
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+text = "don't, touch it"
+first_word = text.split()[0]
+text_without_dots = re.sub(r'$[\w\s]', '', first_word)
+print(text_without_dots)
