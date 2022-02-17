7170e0c301a1d39922ecde9c19c89061be6f4e62
Nikita Sukhonosov <sukhonosov.nikita@gmail.com>
2022-02-17 18:06:22 +0200

    Connect to SQL

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-import re
-#
-#
-# def first_word(text: str) -> str:
-#     """
-#     returns the first word in a given text.
-#     """
-#     # your code here
-#     text_without_dots = re.sub('[$\w\s]', '', text)
-#     return text_without_dots.split()[0]
-#
-#
-# if __name__ == "__main__":
-#     print("Example:")
-#     print(first_word("Hello world"))
-#     print(first_word("don't touch it"))
-#
-#     # # These "asserts" are used for self-checking and not for an auto-testing
-#     # assert first_word("Hello world") == "Hello"
-#     # assert first_word(" a word ") == "a"
-#     # assert first_word("don't touch it") == "don't"
-#     # assert first_word("greetings, friends") == "greetings"
-#     # assert first_word("... and so on ...") == "and"
-#     # assert first_word("hi") == "hi"
-#     # assert first_word("Hello.World") == "Hello"
-#     # print("Coding complete? Click 'Check' to earn cool rewards!")
+from simpledbf import Dbf5
+import pandas as pd
+import pyodbc
 
-text = "don't, touch it"
-first_word = text.split()[0]
-text_without_dots = re.sub(r'$[\w\s]', '', first_word)
-print(text_without_dots)
+# dbf = Dbf5('DH5188.DBF', codec='Windows-1251')
+# dbf.to_csv('DH5188.csv')
+# print(df)
+
+server = 'POWERBIListener'
+database = 'petrykivka_test'
+username = 'sukhonosovn'
+password = 'Fe%f8Pyz'
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID='
+                      + username + ';PWD=' + password)
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM gender')
+
+# df = pd.read_sql_query('SELECT * FROM gender', conn)
