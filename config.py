d32b26777c76bb2bd0bebe6f79152875cb242878
Nikita Sukhonosov <sukhonosov.nikita@gmail.com>
2022-02-21 18:05:33 +0200

    2022/02/21 - Add Config.py and settings.

--- config.py
+++ config.py
@@ -15,12 +15,5 @@ class Config:
     def get_section(self, section):
         return self.configuration.options(section)
 
-    def sqlconfig(self):
-        creds = {}
-        sqlcrededtional = self.configuration.options('SQL')
-        for name in sqlcrededtional:
-            creds[name] = self.get_setting('SQL', name)
-        return creds
-
     def has_section(self, name):
         return self.configuration.has_section(name)
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from tkinter import filedialog
 from connect_to_sql import connector
 from dbf_to_csv import *
-import configparser
+from config import *
 
 if __name__ == '__main__':
     # files_name = filedialog.askopenfilenames()
-    # create_csv_from_dbf(take_dbf_files(files_name))
-    # df = pd.read_csv('DH5188.CSV', encoding='Windows-1251')
-    # dbf = Dbf5('DH5188.DBF', codec='Windows-1251')
-    # print(dbf.to_dataframe())
-    parser = configparser.RawConfigParser()
-    parser.read('settings_Petrykivka.ini')
-    print(parser.sections())
+    config = Config('settings_Petrykivka.ini')
+    print(config.get_section('DT5188'))
