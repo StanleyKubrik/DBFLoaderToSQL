61aadd3290e7c4bb33a3dfdcb21806a7663edc07
Stanley Kubrick <sukhonosov.nikita@gmail.com>
2022-03-07 10:06:49 +0200

    2022/03/05 - Insert to SQL method completed. Need to add check for existing in SQL table keys.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def pad_field_with_spaces(field: str) -> str:
         return str(field).rjust(6) + '   '
 
 
+# Need to debugging check for exist key in SQL table.
 def insert_into_table(dbf_file_from: str, sql_table_to: str):
     try:
         cfg = Config(CONFIG_PATH)  # Initialization cfg object.
@@ -24,18 +25,6 @@ def insert_into_table(dbf_file_from: str, sql_table_to: str):
         dbf_df = dbf.to_dataframe()  # Create simpledbf DataFrame.
         df = pd.DataFrame(dbf_df)  # Converting simpledbf DF to pandas DF.
 
-        # Pad ID fields with spaces to 9 chars.
-        # Create a list with ID fields.
-        id_fields = []
-        for field in dbf.fields:
-            if field[1] == 'C' and field[2] == 9:
-                id_fields.append(field[0])
-        # Appending spaces to each value in the specified column.
-        for col in df.columns:
-            if col in id_fields:  # Check if fields is ID.
-                for v in df[col].values:
-                    df = df.replace({v: pad_field_with_spaces(v.strip())})
-
         # Renaming fields in DataFrame(DBF) according to SQL table and delete fields that don't exist in config-file.
         for col in df.columns:
             if cfg_field_dict.keys().__contains__(col.lower()):
@@ -43,11 +32,30 @@ def insert_into_table(dbf_file_from: str, sql_table_to: str):
             else:
                 df.__delitem__(col)
 
+        # Pad ID fields with spaces to 9 chars.
+        # Create a list with ID fields.
+        id_fields = []
+        for field in dbf.fields:
+            if field[1] == 'C' and field[2] == 9:
+                id_fields.append(field[0])
+
+        # Appending spaces to each value in the specified column.
+        for col in df.columns:
+            if col in id_fields:  # Check if fields is ID.
+                for v in df[col].values:
+                    df = df.replace({v: pad_field_with_spaces(v.strip())})
+
+        sql_table = pd.read_sql_table(sql_table_to, conn, columns=id_fields)
+
         # Writing DataFrame to SQL DB.
         # Check rows count before insert.
         before_ins_rows_count = pd.read_sql_query(f'SELECT COUNT(*) FROM {sql_table_to}', conn).values[0]
         # Inserting DF to SQL.
-        df.to_sql(sql_table_to, conn, if_exists='append', index=False, chunksize=500)
+        for row in df.itertuples(index=False, name=None):
+            if row[0] not in sql_table[id_fields[0]].values:
+                df.to_sql(str(row), conn, if_exists='append', index=False, chunksize=500)
+            else:
+                print(f'Table "{sql_table_to}" already exist key "{row[0]}"!')
         # Check rows count after insert.
         after_ins_rows_count = pd.read_sql_query(f'SELECT COUNT(*) FROM {sql_table_to}', conn).values[0]
         # Calculating and output inserted rows quantity.
