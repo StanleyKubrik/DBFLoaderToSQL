57abcb9ca3c826db8a7dac1195d14879ba045907
Stanley Kubrick <sukhonosov.nikita@gmail.com>
2022-04-17 15:46:47 +0300

    Revert "2022/04/12 - Need to add auto-create table in SQL. Fixing speed of upload."
    This reverts commit 46d6612a

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from interface import MainFrame
 import wx
-from sql import insert_into_sql_table_from_dbf
 
-DBF = 'RA7683.DBF'
 
 if __name__ == '__main__':
-    # app = wx.App()
-    #
-    # frame = MainFrame(None)
-    # frame.Show()
-    #
-    # app.MainLoop()
-    insert_into_sql_table_from_dbf(DBF)
+    app = wx.App()
+
+    frame = MainFrame(None)
+    frame.Show()
+
+    app.MainLoop()
