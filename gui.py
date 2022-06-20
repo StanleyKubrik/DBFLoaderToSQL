406c6138b59ee2c7053c3eae3e55a3a7fab6ec57
Stanley Kubrick <sukhonosov.nikita@gmail.com>
2022-06-20 23:58:13 +0300

    2022/06/20 - Create new functions on gui.

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from gui_qt import Ui_MainWindow
+from os import listdir
 
 
 class GUI(Ui_MainWindow):
@@ -7,8 +9,27 @@ class GUI(Ui_MainWindow):
         self.main_window = QtWidgets.QMainWindow()
         self.setupUi(self.main_window)
 
-        self.btn_browse.clicked.connect(self.browse_directory)
+        self.lineedit_directory.setPlaceholderText('Double-click for open explorer')
+        self.lineedit_directory.selectionChanged.connect(self.browse_directory)
+
+        self.btn_view_files.clicked.connect(self.view_files)
+
+        self.error_select_dir = QMessageBox()
+        self.error_select_dir.setWindowTitle('ERROR')
+        self.error_select_dir.setText('Select a directory first!')
+        self.error_select_dir.setIcon(QMessageBox.Warning)
+        self.error_select_dir.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
     def browse_directory(self):
         directory = QtWidgets.QFileDialog.getExistingDirectory()
         self.lineedit_directory.setText(directory)
+
+    def view_files(self):
+        if self.lineedit_directory is not None:
+            print(listdir(self.lineedit_directory.text()))
+        else:
+            self.error_select_dir.exec_()
+
+    def upload_data(self):
+        for file in self.tbl_dbfs.row():
+            print(file)
