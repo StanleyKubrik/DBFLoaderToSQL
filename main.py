c9b07a3979469c8279b963ca1104bc14c9042f6a
YOUR_NAME <YOUR_EMAIL>
2022-09-21 21:12:02 +0300

    New settings, clear main.py

--- main.py
+++ main.py
@@ -7,12 +7,3 @@ if __name__ == '__main__':
     ui = GUI()
     ui.main_window.show()
     sys.exit(app.exec_())
-
-    # sql = SQL()
-    # for dbf in os.listdir('DBF/'):
-    #     sql.load_into_sql_table_from_dbf(os.getcwd() + '\\DBF\\' + dbf)
-    # sql.load_into_sql_table_from_dbf('C:\\Users\\SUPERMAN\\Desktop\\DBFLoaderToSQL\\DBF\\1SJOURN.DBF')
-    # table = pd.read_sql_table('1SJOURN_test', sql.engine)
-    # vfunc = np.vectorize(sql.from_36_to_time)
-    # table['TIME'] = vfunc(table['TIME'].values)
-    # table.to_sql('1SJOURN_test', sql.engine, if_exists='replace', index=False)
