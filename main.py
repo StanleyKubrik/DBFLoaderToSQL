c68693cea2a5b25162bd9cd7cc32659580d35de4
StanleyKubrik <37335329+StanleyKubrik@users.noreply.github.com>
2022-11-21 14:14:42 +0200

    Added requirements.txt.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication
 from sql import SQL
 import pandas as pd
 from simpledbf import Dbf5
+import os
 
 if __name__ == '__main__':
     # app = QApplication(sys.argv)
@@ -19,9 +20,10 @@ if __name__ == '__main__':
 
     EXCHANGE_CONFIG_PATH = 'settings_Petrykivka.ini'
     exchange_cfg = Config(EXCHANGE_CONFIG_PATH)
+    current_path = os.getcwd()
 
-    dbf_file_from = 'RA7683.DBF'
-    dbf_file_from_path = 'C:\\Users\\User\\Desktop\\DBFLoaderToSQL\\DBF\\RA7683.DBF'
+    dbf_file_from = 'SC4908.DBF'
+    dbf_file_from_path = f'{current_path}\\DBF\\{dbf_file_from}'
     cfg_field_dict = exchange_cfg.get_dict_from_dbf(dbf_file_from)
     # sql_table_to = sql.get_sql_table_name_for_dbf(dbf_file_from)
     # cfg_field_dict = exchange_cfg.get_dict_from_dbf(dbf_file_from)
@@ -29,5 +31,6 @@ if __name__ == '__main__':
     dbf = Dbf5(dbf_file_from_path, codec='1251')
     dbf_df = dbf.to_dataframe()
     df = pd.DataFrame(dbf_df)
-    pd_df = pd.read_table(dbf_file_from_path, encoding='1251')
-    print(pd_df)
+    # print(df.head(10)['ID'].values)
+    for val in df.head(10)['ID']:
+        print(val.replace(' ', '_'))
